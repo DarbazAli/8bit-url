79d1c4737a5ac5516a393c6d568abe9d129780c8
Darbaz Ali <darbaz.me@gmail.com>
2020-11-11 11:21:13 +0300

    add build folder to gitignore

--- backend/express.js
+++ backend/express.js
@@ -16,8 +16,8 @@ config() // dotenv
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(compress())
-app.use(helmet())
-app.use(cors())
+// app.use(helmet())
+// app.use(cors())
 
 console.log(process.env.NODE_ENV)
 
